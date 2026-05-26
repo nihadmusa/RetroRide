7a0a5f3a9cd860df77a76f4b4e5c1bfe645b8e59
nihadmusa <nihad.musa41@gmail.com>
2026-05-27 00:48:47 +0400

    Add production IP to CORS whitelist

--- src/main/java/az/myapp/retroride/config/SecurityConfig.java
+++ src/main/java/az/myapp/retroride/config/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
                 "http://127.0.0.1:5500",
                 "http://localhost:5501",
                 "http://127.0.0.1:5501",
-                "http://ec2-13-53-177-40.eu-north-1.compute.amazonaws.com"
+                "http://ec2-13-53-177-40.eu-north-1.compute.amazonaws.com",
+                "http://ec2-13-53-177-40.eu-north-1.compute.amazonaws.com:8080"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
